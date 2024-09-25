73eb3561a5b95d0dff1847dd6265cf9cb0c466b9
Eand00 <79508332+Eand00@users.noreply.github.com>
2024-09-25 15:07:04 +0200

    Created Master_Trainer project

---
+++ Master_Trainer/src/main/java/com/tjk/MasterTrainerApplication.java
@@ -0,0 +1,13 @@
+package com.tjk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterTrainerApplication.class, args);
+	}
+
+}
