1b67ce9c0af3bb42c19f09d35051c4e341fe30aa
mattemagosso <matteo.magosso@edu.itspiemonte.it>
2024-09-29 17:24:13 +0200

    created UserDAO

---
+++ Master_Trainer/src/main/java/com/tjk/repos/UserDAO.java
@@ -0,0 +1,24 @@
+package com.tjk.repos;
+
+
+import com.tjk.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+/**
+ * The {@code UserDAO} interface provides methods for accessing and manipulating
+ * user entities in the database.
+*/
+public interface UserDAO extends JpaRepository<User, String> {
+
+    /**
+     * Finds a user by username.
+    */
+    Optional<User> findByUsername(String username);
+
+    /**
+     * Finds a user by their id.
+    */
+    Optional<User> findById(String id);
+
+}
