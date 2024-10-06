eb647817f78524f1a64c68ab92462d4a9a5bc5b1
ggiaco-lab <alessio.giacometti@edu.itspiemonte.it>
2024-10-06 16:54:34 +0200

    Modified all methods in DeckCardsServiceImpli

--- Master_Trainer/src/main/java/com/tjk/services/DeckCardsServiceImpl.java
+++ Master_Trainer/src/main/java/com/tjk/services/DeckCardsServiceImpl.java
@@ -2,9 +2,16 @@ package com.tjk.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.tjk.entities.Card;
+import com.tjk.entities.Deck;
 import com.tjk.entities.DeckCards;
 import com.tjk.entities.DeckCardsId;
+import com.tjk.repos.CardDAO;
 import com.tjk.repos.DeckCardsDAO;
+import com.tjk.repos.DeckDAO;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,13 +20,14 @@ public class DeckCardsServiceImpl implements DeckCardsService{
 
     @Autowired
     private DeckCardsDAO deckCardsRepository;
+    private CardDAO cardRepository;
+    private DeckDAO deckRepository;
 
     // Adds a card to a deck with a specified quantity
     @Override
     public void addCardToDeck(Integer deckId, String cardId, Integer quantity) throws IllegalArgumentException {
     	// Checks if the deck has slots available 
-    	int totalCardsInDeck = getTotalCardsInDeck(deckId);
-    	if (totalCardsInDeck == 30) {
+    	if (getTotalCardsInDeck(deckId) == 30) {
     		throw new IllegalArgumentException("The deck is full.");
     	}
     	
@@ -34,8 +42,8 @@ public class DeckCardsServiceImpl implements DeckCardsService{
         
         // Creates an object DeckCards and sets its attributes
         DeckCards deckCards = new DeckCards();
-        deckCards.setIdCard(cardId);
-        deckCards.setIdDeck(deckId);
+        deckCards.setIdCard(cardRepository.findByIdCard(cardId).get());
+        deckCards.setIdDeck(deckRepository.findByIdDeck(deckId).get());
         deckCards.setQuantity(quantity);
 
         // Validate the input data
@@ -64,7 +72,9 @@ public class DeckCardsServiceImpl implements DeckCardsService{
     // Updates the quantity of a card in a deck
     public DeckCards updateCardInDeck(DeckCards deckCards) throws IllegalArgumentException {
     	// Creates a DeckCardsId
-    	DeckCardsId deckCardsId = new DeckCardsId(deckCards.getIdCard(),deckCards.getIdDeck());
+    	String idCard = deckCards.getIdCard().getIdCard();
+    	int idDeck = deckCards.getIdDeck().getIdDeck();
+    	DeckCardsId deckCardsId = new DeckCardsId(idCard,idDeck);
     	
         // Check if the card is in the deck
         Optional<DeckCards> existingDeckCards = deckCardsRepository.findById(deckCardsId);
@@ -81,18 +91,46 @@ public class DeckCardsServiceImpl implements DeckCardsService{
 
     @Override
 	public int getTotalCardsInDeck(Integer deckId) {
-		// TODO Auto-generated method stub
-		return 0;
+    	// Creates a list of DeckCards
+		List<DeckCards> cardList = getCardsInDeck(deckId);
+		
+		// Adds for each card the corresponding quantity to the total
+		int result = 0;
+		for (DeckCards deckCard : cardList) {
+			result += deckCard.getQuantity();
+		}
+    	return result;
 	}
 
 	@Override
 	public boolean isDeckValid(Integer deckId) {
-		// TODO Auto-generated method stub
-		return false;
+		// Creates a list of DeckCards
+		List<DeckCards> cardList = getCardsInDeck(deckId);
+		
+		// Returns false if the deck doesn't contain 30 cards
+		if (getTotalCardsInDeck(deckId) != 30) {
+			return false;
+		}
+		
+		// A list of names of every card in the deck
+		List<String> nameList = new ArrayList<String>(); 
+		for (DeckCards deckCards : cardList) {
+			nameList.add(deckCards.getIdCard().getName());
+		}
+		
+		
+		for (int i = 0; i < cardList.size(); i++) {
+			if (cardList.get(i).getIdCard().getName()==) {
+				
+			}
+		}
+			
+		return true;
+		
 	}
-    //-----------------------------------------------------------------------------------
+	
     // Retrieves all cards in a specific deck
-    public List<DeckCards> getCardsInDeck(String idDeck) {
+    public List<DeckCards> getCardsInDeck(Integer idDeck) {
         return deckCardsRepository.findAllCardsByIdDeck(idDeck);
     }
 
