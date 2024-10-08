18041315cdc6d210da42bea621dc1798a7be8b03
alealiberti <alessandro.aliberti@edu.itspiemonte.it>
2024-10-08 11:50:05 +0200

    Modified CollectionDAO to properly add the method to get favourites

--- Master_Trainer/src/main/java/com/tjk/repos/CollectionDAO.java
+++ Master_Trainer/src/main/java/com/tjk/repos/CollectionDAO.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.tjk.entities.Collection;
 import com.tjk.entities.CollectionId;
@@ -16,7 +17,8 @@ public interface CollectionDAO extends JpaRepository<Collection, CollectionId> {
     // Finds a list of cards in owned by a user.
     List<Collection> findByIdUser(Integer idUser);
     
-    @Query(value="SELECT * FROM collections WHERE favourite=true", nativeQuery = true)
-    List<Collection> findFavouriteByUserId(Integer idUser);
+    // List of Collection of Favourite cards that belong to a certain user, given his idUser
+    @Query(value="SELECT * FROM collections WHERE favourite=true AND id_user=:idUser", nativeQuery = true)
+    List<Collection> findFavouriteByUserId(@Param("idUser") Integer idUser);
 
 }
