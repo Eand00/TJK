8ee1119459fa7b65547ec0ed6456e1ff53d01006
ggiaco-lab <alessio.giacometti@edu.itspiemonte.it>
2024-10-21 09:06:26 +0200

    Updated DeckCardsServiceImpl to check for legalities

--- Master_Trainer/src/main/java/com/tjk/services/DeckCardsServiceImpl.java
+++ Master_Trainer/src/main/java/com/tjk/services/DeckCardsServiceImpl.java
@@ -77,6 +77,9 @@ public class DeckCardsServiceImpl implements DeckCardsService{
 
         // Remove the card from the deck
         deckCardDao.delete(existingDeckCards.get());
+        
+        // Checks if after removing a card the deck is valid
+        existingDeckCards.get().getDeck().setLegal(isDeckValid(deckId));
     }
 
     // Updates the quantity of a card in a deck
@@ -103,6 +106,9 @@ public class DeckCardsServiceImpl implements DeckCardsService{
 
         // Validate the updated data
         validateDeckCards(existingDeckCards);
+        
+        // Checks if after updating a card's quantity the deck is valid
+        existingDeckCards.getDeck().setLegal(isDeckValid(deckId));
 
         // Save the updated DeckCards entry to the database
         return deckCardDao.save(existingDeckCards);
